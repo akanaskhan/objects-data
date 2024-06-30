a9c55103ad335f09a26f84adea6733411c0a34f4
akanaskhan <muhammadanaskhanak@gmail.com>
2024-06-30 20:58:28 +0500

    updated

---
+++ app.js
@@ -0,0 +1,84 @@
+var cars = {
+    honda: {
+        civic: {
+            name: "Civic 2023",
+            price: 7800000,
+            color: ['red', 'black', 'white'],
+            custom: "paid",
+            taxes: "paid",
+        },
+        accord: {
+            name: "Accord 2023",
+            price: 9500000,
+            color: ['blue', 'gray', 'white'],
+            custom: "unpaid",
+            taxes: "paid",
+        }
+    },
+    toyota: {
+        corolla: {
+            name: "Corolla 2023",
+            price: 7600000,
+            color: ['black', 'white', 'silver'],
+            custom: "paid",
+            taxes: "unpaid",
+        },
+        camry: {
+            name: "Camry 2023",
+            price: 11200000,
+            color: ['red', 'black', 'blue'],
+            custom: "paid",
+            taxes: "paid",
+        }
+    },
+    ford: {
+        mustang: {
+            name: "Mustang 2023",
+            price: 15000000,
+            color: ['yellow', 'black', 'red'],
+            custom: "paid",
+            taxes: "unpaid",
+        },
+        explorer: {
+            name: "Explorer 2023",
+            price: 13000000,
+            color: ['blue', 'black', 'white'],
+            custom: "unpaid",
+            taxes: "paid",
+        }
+    },
+    bmw: {
+        series3: {
+            name: "3 Series 2023",
+            price: 14000000,
+            color: ['white', 'black', 'gray'],
+            custom: "paid",
+            taxes: "paid",
+        },
+        series5: {
+            name: "5 Series 2023",
+            price: 18000000,
+            color: ['blue', 'black', 'silver'],
+            custom: "paid",
+            taxes: "paid",
+        }
+    },
+    audi: {
+        a4: {
+            name: "A4 2023",
+            price: 13500000,
+            color: ['red', 'black', 'white'],
+            custom: "paid",
+            taxes: "unpaid",
+        },
+        q7: {
+            name: "Q7 2023",
+            price: 20000000,
+            color: ['gray', 'black', 'blue'],
+            custom: "unpaid",
+            taxes: "paid",
+        }
+    }
+};
+
+console.log(cars);
